7d65f328cde97c12113a96db8c41beefa035ab66
William Mittard <billymittard@ymail.com>
2020-04-15 14:32:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 def badge_maker(name)
-  return "Hello my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
-  array.map { |i| badge_maker (i)}
+    array.map { |i| badge_maker(i)}
 end
